f6d3854c2f013a61e0b5f7de710ca28cb7bd1966
Ryan Kois <ryan.kois@gmail.com>
2014-03-27 14:57:16 -0700

    Idk.

--- vagrantfile/manifests/default.pp
+++ vagrantfile/manifests/default.pp
@@ -3,8 +3,6 @@ exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
 Exec["apt-update"] -> Package <| |>
-
-# Global path for any execs we wanna run.
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 # Apache
@@ -14,33 +12,14 @@ class {'apache':
 apache::mod { 'rewrite':  }
 apache::mod { 'php5':  }
 
-apache::vhost {'your-project':
-  # MODIFY FOR YOUR PROJECT!
-  servername => 'your.project.com',
-  docroot  => '/var/www/your.project.com',
+file {'/var/www/drupal':
+  ensure => 'directory',
+} ->
+apache::vhost {'drupal':
+  servername => 'drupal.dev',
+  docroot  => '/var/www/drupal',
   override => 'all',
-  docroot_owner => 'vagrant',
-  docroot_group => 'vagrant',
   port          => 80
-}
-
-# Change apache user
-exec { "ApacheUserChange" :
-    command => "sed -i 's/APACHE_RUN_USER=www-data/APACHE_RUN_USER=vagrant/' /etc/apache2/envvars",
-    onlyif  => "grep -c 'APACHE_RUN_USER=www-data' /etc/apache2/envvars",
-    require => Package["apache2"],
-    notify  => Service["apache2"],
-}
-exec { "ApacheGroupChange" :
-    command => "sed -i 's/APACHE_RUN_GROUP=www-data/APACHE_RUN_GROUP=vagrant/' /etc/apache2/envvars",
-    onlyif  => "grep -c 'APACHE_RUN_GROUP=www-data' /etc/apache2/envvars",
-    require => Package["apache2"],
-    notify  => Service["apache2"],
-}
-exec { "apache_lockfile_permissions" :
-    command => "chown -R vagrant:www-data /var/lock/apache2",
-    require => Package["apache2"],
-    notify  => Service["apache2"],
 }
 
 # MySQL
@@ -53,14 +32,12 @@ class { 'mysql::server':
 mysql::db { 'drupaldb':
   user => 'drupal',
   password => 'drupalsql',
-  # If you want to import a db, comment out the following:
-  # sql => '/sql/example.sql'
+} ->
+class {'php':} ->
+class {'drush':} ->
+exec{'drush dl drupal --drupal-project-rename --destination=/var/www':
+  creates => '/var/www/drupal/index.php'
+} ->
+exec {'drush site-install  --db-url=mysqli://drupal:drupalsql@localhost/drupaldb':
+  creates => '/var/www/drupal/index.php'
 }
-
-# PHP
-class {'php':}
-# If you need memcache, comment out the following line:
-# ->  class {'memcache':}
-
-# Drush
-class {'drush':}
