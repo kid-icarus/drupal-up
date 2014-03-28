4578694e24774489b256fe0150b6fd963fb697ab
Ryan Kois <ryan.kois@gmail.com>
2014-03-27 17:58:49 -0700

    Add uploadprogress for all greenz.

--- vagrantfile/modules/php/manifests/init.pp
+++ vagrantfile/modules/php/manifests/init.pp
@@ -1,4 +1,13 @@
 class php () {
-  $packages = ['libapache2-mod-php5', 'php5-common', 'php5-cli', 'php5-curl', 'php5-mysql', 'php5-gd', 'php-pear']
-  package { $packages : ensure => 'installed'}
+  $packages = ['libapache2-mod-php5', 'php5-common', 'php5-cli', 'php5-curl', 'php5-mysql', 'php5-gd', 'php-pear', 'build-essential']
+  package { $packages :
+    ensure => 'installed'
+  }
+  -> exec {'pecl install uploadprogress':
+    creates => '/usr/lib/php5/20090626/uploadprogress.so'
+  }
+  -> exec {'echo extension=uploadprogress.so >> /etc/php5/apache2/php.ini':
+    unless  => "grep -c 'extension=uploadprogress.so' /etc/php5/apache2/php.ini",
+    notify => Service['apache2']
+  }
 }
